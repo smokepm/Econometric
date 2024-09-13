29420f2589c453c056c1cb1513f46dd9e4d17e56
Parker <parkermorrow19@gmail.com>
2024-09-12 17:28:39 -0700

    notebook

--- intro.r
+++ intro.r
@@ -1,5 +1,12 @@
 library(readxl)
+library(ggplot2)
 
 data <- read_excel("files/foodexp.xls")
 
-head(data, 5)
+linear_model <- lm(FOODEXP ~ INCOME, data = data)
+
+print(summary(linear_model))
+
+ggplot(data, aes(INCOME, FOODEXP)) +
+  geom_point() +
+  geom_smooth(method = "lm")
